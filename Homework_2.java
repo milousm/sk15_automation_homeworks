8ca2c1785843eafd6b9e1487aea1fdbab12df902
Margarita Milousheva <margarita.milousheva@mentormate.com>
2024-08-15 11:39:25 +0300

    added Homework#2 branch

---
+++ Homework_2.java
@@ -0,0 +1,243 @@
+import java.util.Scanner;
+
+public class Homework_2 {
+    public static void main(String[] args) {
+        possibleTriangle();
+        daysOfTheWeek();
+        workingAge();
+        revenueCalculator();
+        destinationAdvisor();
+        leapYearCheck();
+        temperatureConverter();
+    }
+
+    public static void possibleTriangle() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter angle 'a' for triangle (max 7 decimal digits)");
+        float a = scanner.nextFloat();
+
+        System.out.print("Enter angle 'b' for triangle (max 7 decimal digits)");
+        float b = scanner.nextFloat();
+
+        System.out.print("Enter angle 'c' for triangle (max 7 decimal digits)");
+        float c = scanner.nextFloat();
+
+        // create a variable that later will help us calculate triangle type
+        boolean possibleTriangle = false;
+
+        // first we check if triangle is possible, if not we display error message to user
+        if (a + b + c != 180) {
+            System.out.print("Triangle is not possible");
+        }
+         else {
+             // once we establish the triangle is possible we check its type according to its angles
+                possibleTriangle = true;
+             if (a == 90 || b == 90 || c == 90) {
+                System.out.print("Triangle is right-angled");
+            } else if (a > 90 || b > 90 || c > 90) {
+                System.out.print("Triangle is Obtuse");
+            } else {
+                System.out.print("Triangle is Acute");
+            }
+        }
+
+         // after finding the triangle's type by its angles we check its type by sides
+         if (possibleTriangle == true) {
+            if (a == b && b == c) {
+                System.out.print(" and  equilateral");
+            } else if (a == b || b == c || a == c) {
+                System.out.print(" and isosceles");
+            } else {
+                System.out.print(" and multifaceted");
+            }
+
+        }
+    }
+
+
+    public static void daysOfTheWeek() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number from 1 to 7");
+        byte day = scanner.nextByte();
+
+        // we check the input number and compare it to the days of the week
+        switch (day) {
+            case 1:
+                System.out.print("The " + day + "-st day of the week is Monday");
+                break;
+            case 2:
+                System.out.print("The " + day + "-nd day of the week is Tuesday");
+                break;
+            case 3:
+                System.out.print("The " + day + "-rd day of the week is Wednesday");
+                break;
+            case 4:
+                System.out.print("The " + day + "-th day of the week is Thursday");
+                break;
+            case 5:
+                System.out.print("The " + day + "-th day of the week is Friday");
+                break;
+            case 6:
+                System.out.print("The " + day + "-th day of the week is Saturday");
+                break;
+            case 7:
+                System.out.print("The " + day + "-th day of the week is Sunday");
+                break;
+            default:
+                System.out.print("The week has only 7 days");
+        }
+
+    }
+
+    public static void workingAge() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        byte age = scanner.nextByte();
+
+        // we check if the use is between 16 and 65 (eligible working age)
+        if (age >= 16) {
+            if (age > 65) {
+                System.out.print("You are above the working age");
+            } else {
+                System.out.print("You are eligible to work");
+            }
+        } else {
+            System.out.print("You are below the working age");
+        }
+    }
+
+    public static void revenueCalculator() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter unit price: ");
+        double price = scanner.nextDouble();
+
+        System.out.print("Enter unit quantity: ");
+        int quantity = scanner.nextInt();
+
+        // creating variables that will help us with calculations
+        double total = (price * quantity);
+        double discountRate1 = 0.15;
+        double discountRate2 = 0.20;
+        double discountTotal = 0;
+
+        if (quantity < 100) {
+            // checking if quantity is below required for discount
+            System.out.print("The revenue from sale:" + " " + (total - discountTotal) + " and discount:" + " " + discountTotal + "(0%)" );
+        } else if (quantity >= 100 && quantity <= 120) {
+            // apply the first discounted rate
+            discountTotal = (total * discountRate1);
+            System.out.print("The revenue from sale:" + " " + (total - discountTotal) + " and discount:" + " " + discountTotal + "(15%)"  );
+        } else {
+            // apply the second discounted rate
+            discountTotal = (total * discountRate2);
+            System.out.print("The revenue from sale:" + " " + (total - discountTotal) + " and discount:" + " " + discountTotal + "(20%)" );
+        }
+    }
+
+
+    public static void destinationAdvisor(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter vacation budget: ");
+        double budget = scanner.nextFloat();
+
+        System.out.print("Enter vacation days: ");
+        int days = scanner.nextInt();
+
+        System.out.print("Enter number of people: ");
+        int people = scanner.nextInt();
+
+        System.out.print("Enter vacation type (Mountain or Beach): ");
+        String type = scanner.next();
+
+        // calculating the budget per day from input values
+        double budgetPerDay = budget / (days * people);
+
+        // there are two ways to show the user the required information
+        // first way with "if"
+        if (budgetPerDay < 50 && type.equals("Beach")) {
+            System.out.print("Ideal destination: Bulgaria");
+        } else if (budgetPerDay >= 50 && type.equals("Beach")) {
+            System.out.print("Ideal destination: Outside Bulgaria");
+        } else if (budgetPerDay < 30 && type.equals("Mountain")) {
+            System.out.print("Ideal destination: Bulgaria");
+        } else if (budgetPerDay >= 30 && type.equals("Mountain")) {
+            System.out.print("Ideal destination: Outside Bulgaria");
+        } else {
+            System.out.print("No information on this type of vacation");
+        }
+
+        //second way with switch case
+        switch (type) {
+            case "Beach":
+                if (budgetPerDay < 50) {
+                    System.out.println("Ideal destination: Bulgaria");
+                } else {
+                    System.out.println("Ideal destination: Outside Bulgaria");
+                }
+                break;
+            case "Mountain":
+                if (budgetPerDay < 30) {
+                    System.out.println("Ideal destination: Bulgaria");
+                } else {
+                    System.out.println("Ideal destination: Outside Bulgaria");
+                }
+                break;
+            default:
+                System.out.println("No information on this type of vacation");
+        }
+    }
+
+    public static void leapYearCheck() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        // creating a variable that will hold the value if the year is leap or not
+        boolean leapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                leapYear = true;
+            }
+        }
+
+        if (leapYear) {
+            System.out.println("Year is leap year");
+        } else {
+            System.out.println("Year is not a leap year");
+        }
+    }
+
+    public static void temperatureConverter() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the temperature: ");
+        double temperature = scanner.nextDouble();
+
+        System.out.print("Convert to Fahrenheit or  Celsius?: ");
+        String temp = scanner.next();
+
+        // Perform the conversion based on the user's choice using a switch statement
+        switch (temp){
+            case ("Fahrenheit"):
+                // convert Celsius to Fahrenheit
+                double fahrenheit = (temperature * 9 / 5) + 32;
+                System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+                break;
+            case ("Celsius"):
+                // convert Fahrenheit to Celsius
+                double celsius = (temperature - 32) * 5 / 9;
+                System.out.println("Temperature in Celsius: " + celsius);
+                break;
+            default:
+                System.out.println("Incorrect input");
+                break;
+        }
+    }
+}
